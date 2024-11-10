420ed9e8dba7647b60c9cc84f74d6ec422d37d74
Gorciu <gorciuyt@gmail.com>
2024-11-10 11:25:08 +0100

    connection state

--- src/brooklen/script.ts
+++ src/brooklen/script.ts
@@ -4,3 +4,30 @@
  * 
  * (c) 2024 Gorciu
  */
+
+function main() {
+    var element_state_loading = document.querySelector('.load-state');
+    if (!element_state_loading) { return; }
+
+    document.title = 'Brooklen - connecting (0% - setting up connection)';
+    element_state_loading.textContent = '0% - setting up connection';
+    if (!navigator.onLine) {
+        document.title = 'Brooklen - connecting canceled';
+        element_state_loading.textContent = 'canceled; restart the application when you will receive Internet access';
+        return;
+    }
+
+    document.title = 'Brooklen - connecting (50% - checking repository)';
+    element_state_loading.textContent = '50% - checking repository';
+    var is_ok : boolean = false;
+    fetch('https://raw.githubusercontent.com/gorciu-official/BrooklenIndex/refs/heads/main/list.json').then((fetched) => {
+        is_ok = fetched.ok;
+    })
+    if (!is_ok) {
+        document.title = 'Brooklen - connecting canceled';
+        element_state_loading.textContent = 'canceled; detected that Brooklen is blocked';
+        return;
+    }
+}
+
+document.addEventListener('DOMContentLoaded', main);
